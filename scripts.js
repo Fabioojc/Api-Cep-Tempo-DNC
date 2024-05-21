05c6a82e797f0a237692d74cb9235283ff282aa2
Fabioojc <136731735+Fabioojc@users.noreply.github.com>
2024-05-21 18:43:17 -0300

    Atualizações.
    Atualizações.

--- scripts.js
+++ scripts.js
@@ -2,14 +2,30 @@ const chave = "viacep.com.br"
 const Tempo = url = "https://api.open-meteo.com/v1/forecast?latude=-10&longitude-55"
 
 
+const handleSubmit = (event) => {        //Evento para Não deixar a pagina atualizar
+  event.preventDefault();
 
+  const name = document.querySelector('input[name=name]').value
+  const email = document.querySelector('input[name=email]').value
+
+
+  fetch('https://api.sheetmonkey.io/form/cxrGEmpwnpbRyUAYSSTZ45', {
+
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ name, email }),  //JSON.Stringify-> vai converter um objeto em estringi
+
+
+  });
+}
 
 async function buscarCep(cep) {
   const dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then(resposta => resposta.json())
   colocarDadosNaTela(dados);
 };
-
-
 
 
 
@@ -27,38 +43,17 @@ function cliqueiNoBotao() {
   const longitude = document.querySelector("#longitude").value
   buscarCep(cep)
   buscarTempo(latitude, longitude)
-
 };
 
 
 async function buscarTempo(latitude, longitude) {
   const dados = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}.41&hourly=temperature_2m&timeformat=unixtime&forecast_days=1`).then(resposta => resposta.json());
   console.log(dados)
-
   document.querySelector(".nume-graus").innerHTML = dados.hourly.temperature_2m[0];
 }
 
-
-const handleSubmit = (event) => {        //Evento para Não deixar a pagina atualizar
-  event.preventDefault();
-
-  const name = document.querySelector('input[name=name]').value
-  const email = document.querySelector('input[name=email]').value
-
-
-  fetch('https://api.sheetmonkey.io/form/cxrGEmpwnpbRyUAYSSTZ45', {
-
-    method: "post",
-    headers: {
-      'Accept': 'application/json',
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ name, email }),  //JSON.Stringify-> vai converter um objeto em estringi
-
-  });
-}
-
 document.querySelector('form').addEventListener('submit', handleSubmit);
+
 
 
 // async avisando que vai acessar um servidor
